3f4e39499fa137f055d1e42d1797cf59768e451e
Arthur Lima <arthur.lima@ijunior.com>
2020-02-07 18:05:14 -0300

    extracted code to function

--- spacifier.js
+++ spacifier.js
@@ -1,8 +1,10 @@
+function _spacify(str){ return str.split('').join(' ');}
+
 const spacify = (str) => {
-	let splitResult = str.split('..');
-	if (splitResult.length === 1) return splitResult[0].split('').join(' ');
-	for (let i=0; i< splitResult.length; i++)
-		if(i%2 === 1) splitResult[i] = splitResult[i].split('').join(' ');
-	return splitResult.join(' ');
+    let splitResult = str.split('..');
+    if (splitResult.length === 1) return _spacify(splitResult);
+    for (let i=0; i< splitResult.length; i++)
+        if(i%2 === 1) splitResult[i] = _spacify(splitResult);
+    return splitResult.join(' ');
 }
 module.exports = spacify;
